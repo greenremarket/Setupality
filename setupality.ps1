649e47e8f49e9953eff6b7aa41a479d434081b1a
Morad IGMIR <52404884+moradigmir@users.noreply.github.com>
2023-05-20 01:29:55 +0200

    accept the damn agreements

--- setupality.ps1
+++ setupality.ps1
@@ -18,22 +18,34 @@ Get-WUInstall -MicrosoftUpdate -AcceptAll -AutoReboot | Out-Null
 # Install PSReadLine
 Install-Module -Name PSReadLine -Force -Confirm:$false
 
-# Install winget
-Invoke-WebRequest -Uri https://github.com/microsoft/winget-cli/releases/download/v1.0.11692/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle -OutFile winget.appxbundle
-Add-AppxPackage .\winget.appxbundle
+# Install winget# get latest download url
+$URL = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
+$URL = (Invoke-WebRequest -Uri $URL).Content | ConvertFrom-Json |
+        Select-Object -ExpandProperty "assets" |
+        Where-Object "browser_download_url" -Match '.msixbundle' |
+        Select-Object -ExpandProperty "browser_download_url"
+
+# download
+Invoke-WebRequest -Uri $URL -OutFile "Setup.msix" -UseBasicParsing
+
+# install
+Add-AppxPackage -Path "Setup.msix"
+
+# delete file
+Remove-Item "Setup.msix"
 
 # Install apps with winget
-winget install --id=Git.Git --exact # git
-winget install --id=GitHub.GitHubDesktop --exact # github desktop
-winget install --id=Valve.Steam --exact # steam
-winget install --id=BraveSoftware.BraveBrowser --exact # brave
-winget install --id=Spotify.Spotify --exact # spotify
-winget install --id=Microsoft.VisualStudioCode --exact # visual studio code
+winget install --id=Git.Git --exact --accept-source-agreements --accept-package-agreements # git
+winget install --id=GitHub.GitHubDesktop --exact --accept-source-agreements --accept-package-agreements # github desktop
+winget install --id=Valve.Steam --exact --accept-source-agreements --accept-package-agreements # steam
+winget install --id=BraveSoftware.BraveBrowser --exact --accept-source-agreements --accept-package-agreements # brave
+winget install --id=Spotify.Spotify --exact --accept-source-agreements --accept-package-agreements # spotify
+winget install --id=Microsoft.VisualStudioCode --exact --accept-source-agreements --accept-package-agreements # visual studio code
 
 # Install Python and Ruby
-winget install --id=Python.Python --exact # python
-winget install --id=RubyInstallerTeam.Ruby --exact # ruby
-winget install --id=9PC3H3V7Q9CH --exact # rufus
+winget install --id=Python.Python --exact --accept-source-agreements --accept-package-agreements # python
+winget install --id=RubyInstallerTeam.Ruby --exact --accept-source-agreements --accept-package-agreements # ruby
+winget install --id=9PC3H3V7Q9CH --exact --accept-source-agreements --accept-package-agreements # rufus
 
 # Install VS Code extensions
 # You would need to list out all your extensions here
